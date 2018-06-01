e6cb59b27e576a90f7eb7f23a1d536cd6f14e383
Khalid Hossain <35842573+Khalid-94@users.noreply.github.com>
2018-06-01 22:12:35 +0600

    Add files via upload
    I made this online newspaper with my webtech group.

---
+++ about.php
@@ -0,0 +1,76 @@
+<?php  
+	require_once("commonCode.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About Us!!!</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="CSS/commonCode.css">
+	<link rel="stylesheet" type="text/css" href="CSS/about.css">
+</head>
+<body>
+	<div id="main_body">
+		<?php  TITLE_LOGO(); ?>
+		<?php NAVIGATION_BAR(); ?>
+		<div class="card_container">
+			<div class="typewritter">
+				<h1>Meet The Team</h1>
+			</div>
+			<div class="card">
+				<img class="card_img" src="IMG/khalid.jpg" alt="khalid">
+				<h1>Khalid Hossain</h1>
+				<p class="title">Student</p>
+				<p>American International University - Bangladesh</p>
+				<div style="margin: 24px 0;">
+					<a href="https://twitter.com/Ami_Arko" target="_blank"><img border="0" alt="twitter" src="IMG/twitter.png" width="25" height="25"></a>  
+					<a href="https://www.facebook.com/yeasir.yeasin.9" target="_blank"><img border="0" alt="facebook" src="IMG/facebook.png" width="25" height="25"></a>
+					<a href="https://www.linkedin.com/in/khalid-hossain-arko-528920122/" target="_blank"><img border="0" alt="linkedin" src="IMG/linkedin.png" width="25" height="25"></a>  
+				</div>
+			</div>
+			<div class="card">
+				<img class="card_img" src="img/promi.jpg" alt="promi">
+				<h1>Noshin Tabassum</h1>
+				<p class="title">Student</p>
+				<p>American International University - Bangladesh</p>
+				<div style="margin: 24px 0;"> 
+					<a href="https://www.facebook.com/tabassum.promi" target="_blank"><img border="0" alt="facebook" src="IMG/facebook.png" width="25" height="25"></a>
+					<a href="https://www.linkedin.com/in/tabassum-promi-470ab0141/" target="_blank"><img border="0" alt="linkedin" src="IMG/linkedin.png" width="25" height="25"></a>  
+				</div>
+			</div>
+			<div class="card">
+				<img class="card_img" src="img/borhan.jpg" alt="borhan">
+				<h1>Borhan Uddin</h1>
+				<p class="title">Student</p>
+				<p>American International University - Bangladesh</p>
+				<div style="margin: 24px 0;">
+					<a href="https://twitter.com/Mdborhan1996" target="_blank"><img border="0" alt="twitter" src="IMG/twitter.png" width="25" height="25"></a>  
+					<a href="https://www.facebook.com/borhan.uddin.1996" target="_blank"><img border="0" alt="facebook" src="IMG/facebook.png" width="25" height="25"></a>
+					<a href="https://www.linkedin.com/in/md-borhan-600b23101/" target="_blank"><img border="0" alt="linkedin" src="IMG/linkedin.png" width="25" height="25"></a>  
+				</div>
+			</div>
+			<div class="card">
+				<img class="card_img" src="img/novera.jpg" alt="novera">
+				<h1>Novera Alam</h1>
+				<p class="title">Student</p>
+				<p>American International University - Bangladesh</p>
+				<div style="margin: 24px 0;"> 
+					<a href="https://www.facebook.com/novera.alam" target="_blank"><img border="0" alt="facebook" src="IMG/facebook.png" width="25" height="25"></a> 
+				</div>
+			</div>
+			<div class="card">
+				<img class="card_img" src="img/turin.jpg" alt="Rakib">
+				<h1>Rakibul Islam</h1>
+				<p class="title">Student</p>
+				<p>American International University - Bangladesh</p>
+				<div style="margin: 24px 0;">
+					<a href="https://twitter.com/sheikh2rin" target="_blank"><img border="0" alt="twitter" src="IMG/twitter.png" width="25" height="25"></a>  
+					<a href="https://www.facebook.com/2rind" target="_blank"><img border="0" alt="facebook" src="IMG/facebook.png" width="25" height="25"></a>
+					<a href="https://www.linkedin.com/in/sheikhrakib/" target="_blank"><img border="0" alt="linkedin" src="IMG/linkedin.png" width="25" height="25"></a>
+				</div>
+			</div>
+		</div>
+		<?php FOOTER(); ?>
+	</div>
+</body>
+</html>
